53142d3a7bff4823611bb2c4a42f2a971385b08c
tiancf <1163586133@qq.com>
2023-01-07 11:23:07 +0800

    项目基础搭建 - 路由、main.js、字体、基础组件

--- vue-my-shop/src/router/index.js
+++ vue-my-shop/src/router/index.js
@@ -1,29 +1,26 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
+  {path: '/', redirect: '/login'},
+  {path: '/login', component: () => import('@/components/Login.vue')},
   {
-    path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '/home', 
+    component: () => import('@/components/Home.vue'),
+    redirect: '/welcome',
+    children: [
+      {path: '/welcome', component: () => import('@/components/Welcome.vue')},
+      {path: '/users', component: () => import('@/components/user/Users.vue')}
+    ]
   }
 ]
 
 const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
-  routes
+  routes: routes
 })
 
 export default router
